3d8b00da54d03a43e4f066c48fb262c8447dd395
Jacob A. Bruinsma <me@jacobbruinsma.com>
2026-05-20 11:24:21 -0500

    fixed config problem, now writing config errors to log

--- webServer/config.py
+++ webServer/config.py
@@ -1,5 +1,7 @@
-import json
+from log import log
 from typing import Any
+import json
+import traceback
 
 
 cached = False
@@ -9,9 +11,15 @@ config_cache: dict[str, Any] = {}
 def config(name: str) -> Any:
 	global cached, config_cache
 
-	if not cached:
-		with open('config.json', 'r') as f:
-			config_cache = json.load(f)
-		cached = True
+	try:
 
-	return config_cache[name]
+		if not cached:
+			with open('config.json', 'r') as f:
+				config_cache = json.load(f)
+			cached = True
+
+		return config_cache[name]
+
+	except:
+		log(traceback.format_exc())
+		raise
